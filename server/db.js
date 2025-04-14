e6b8e1cfa08b80d3a833fef57852c9a8b843b4a4
Fatma Saafan <fas3030@yahoo.com>
2025-04-13 21:18:56 -0400

    added server folder-login file-modified index.js

---
+++ server/db.js
@@ -0,0 +1,9 @@
+// Simple DB config 
+const conn_info = {
+    host: 'localhost',
+    user: 'root',
+    password: 'your_mysql_password', // <-- replace with your real password
+    database: 'attendance_db'
+  };
+  
+  module.exports = { conn_info };
